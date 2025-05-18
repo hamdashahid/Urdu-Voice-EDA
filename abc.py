d06a619cd5d027436bdc4e970fc60c9d68b9016b
hamdashahid <hamdashahid5050@gmail.com>
2025-05-18 15:15:43 +0500

    files upload

---
+++ abc.py
@@ -0,0 +1,13 @@
+## q1 code
+import pandas as pd
+
+# Ensure the file path is correct
+clipDuration = pd.read_csv('cv-corpus-19.0-2024-09-13/ur/clip_durations.tsv', sep='\t')
+clipDuration
+
+
+
+## q1 code
+import pandas as pd
+clipDuration = pd.read_csv('cv-corpus-19.0-2024-09-13\ur\clip_durations.tsv', sep='\t')
+clipDuration
